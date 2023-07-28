5adbb028cf3738c2d2c5452ee8d793ad843828a9
AlkaMalik <malikalka2090@gmail.com>
2023-07-28 23:58:20 +0530

    added property api

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingServiceService } from 'src/app/services/housing-service.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+  properties: any = [];
 
+  constructor( private housingService: HousingServiceService  ) {
+  }
+  ngOnInit(): void {
+    this.housingService.getAllProperties()
+    .subscribe(response=>{
+      this.properties = response;
+    }, 
+    error=>{
+      console.log(error)
+    })
+  }
 }
